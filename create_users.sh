bee933da0777f17d6843eef1e05f261ba6df5f30
iNDECxdd <142155520+iNDECxdd@users.noreply.github.com>
2026-05-10 15:53:21 +0200

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
 
 #Lägger till mappar i skel som nya användare får automatiskt
-sudo mkdir /etc/skel/Downloads
-sudo mkdir /etc/skel/Documents
-sudo mkdir /etc/skel/Work
+Downloads = "/etc/skel/Downloads"
+Documents = "/etc/skel/Documents"
+Work = "/etc/skel/Work"
+
+if test ! -d "$Downloads"
+then
+    sudo mkdir /etc/skel/Downloads
+fi
+
+if test ! -d "$Documents"
+then
+    sudo mkdir /etc/skel/Documents
+fi
+
+if test ! -d "$Work"
+then
+    sudo mkdir /etc/skel/Work
+fi
 
 #Skapar nya användare, for-loop används för att gå igenom alla parametrar i $@
 for användarnamn in "$@"
 do 
-    sudo useradd "$användarnamn"
+    sudo useradd -m "$användarnamn"
 done
