47eb771731fd3239df9880682415900fcb89c5c4
iNDECxdd <142155520+iNDECxdd@users.noreply.github.com>
2026-05-10 20:05:34 +0200

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -12,25 +12,25 @@ for USERNAME in "$@"
 do 
     #Skapar användare med en hemkatalog
     useradd -m "$USERNAME"
-    homedir="/home/$USERNAME"
-    welcome="$homedir/welcome.txt"
+    HOME="/home/$USERNAME"
+    WELCOME="$HOME/welcome.txt"
 
     #Skapar mappar
-    mkdir -p "$homedir/Downloads"
-    mkdir -p "$homedir/Documents"
-    mkdir -p "$homedir/Work"
+    mkdir -p "$HOME/Downloads"
+    mkdir -p "$HOME/Documents"
+    mkdir -p "$HOME/Work"
 
     #Ger ägande över hemkatalogen till användaren
-    chown -R "$USERNAME:$USERNAME" "$homedir"
+    chown -R "$USERNAME:$USERNAME" "$HOME"
 
     #Bara användaren kommer åt hemkatalogen
-    chmod -R 700 "$homedir"
+    chmod -R 700 "$HOME"
 
-    #Lägger till användaren till välkomstfilen
-    echo "Välkommen $USERNAME" >"$welcome"
-    echo "Övriga användare:" >> "$welcome"
-
-    #Ändrar rättigheter så alla kan läsa filen
-    chmod 644 "$welcome"
-
+    #Skapar en användarlista med hjälp av en filtrering av /etc/passw där alla övriga användare med UID 1000 eller högre är med (vanliga användare)
+    echo "Välkommen $USERNAME" > "$WELCOME"
+    echo "Övriga användare:" >> "$WELCOME"
+    awk -F: '$3 >= 1000 {print $1}' /etc/passwd | grep -v "$USERNAME" >> "$WELCOME"
+    
+    #Ändrar rättigheter så alla kan läsa filen men bara ägaren kan ändra den.
+    chmod 644 "$WELCOME"
 done
