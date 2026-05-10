1cb2d6439a1a8153aa9e9ce1d380273825599188
iNDECxdd <142155520+iNDECxdd@users.noreply.github.com>
2026-05-10 17:01:55 +0200

    Enhance create_users.sh with comments and permissions
    Added comments for clarity and set permissions for welcome file.

--- create_users.sh
+++ create_users.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 #Kollar om användaren är root eller inte
-
 if [ "$EUID" -ne 0 ];
 then
     echo "Du har ej behörighet att köra detta script"
@@ -11,14 +10,27 @@ fi
 #Skapar nya användare och mappar, for-loop används för att gå igenom alla parametrar i $@
 for username in "$@"
 do 
+    #Skapar användare med en hemkatalog
     useradd -m "$username"
-
     homedir="/home/$username"
     welcome="$homedir/welcome.txt"
 
+    #Skapar mappar
     mkdir -p "$homedir/Downloads"
     mkdir -p "$homedir/Documents"
     mkdir -p "$homedir/Work"
 
+    #Ger ägande över hemkatalogen till användaren
     chown -R "$username:$username" "$homedir"
+
+    #Bara användaren kommer åt hemkatalogen
+    chmod -R 700 "$homedir"
+
+    #Lägger till användaren till välkomstfilen
+    echo "Välkommen $username" >"$welcome"
+    echo "Övriga användare:" >> "$welcome"
+
+    #Ändrar rättigheter så alla kan läsa filen
+    chmod 644 "$welcome"
+
 done
