0feedac6d4f82a3824d4f27ec6636cc01ef0d0b6
iNDECxdd <142155520+iNDECxdd@users.noreply.github.com>
2026-05-10 16:13:18 +0200

    Update create_users.sh

--- create_users.sh
+++ create_users.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+#Kollar om användaren är root eller inte
+
+if ["$EUID" -ne 0 ];
+then
+    echo "Du har ej behörighet att köra detta script"
+    exit 1
+fi
+
+#Skapar nya användare, for-loop används för att gå igenom alla parametrar i $@
+for användarnamn in "$@"
+do 
+    sudo useradd -m "$användarnamn"
+done
+
 #Lägger till mappar i skel som nya användare får automatiskt
 Downloads = "/etc/skel/Downloads"
 Documents = "/etc/skel/Documents"
@@ -20,8 +34,3 @@ then
     sudo mkdir /etc/skel/Work
 fi
 
-#Skapar nya användare, for-loop används för att gå igenom alla parametrar i $@
-for användarnamn in "$@"
-do 
-    sudo useradd -m "$användarnamn"
-done
