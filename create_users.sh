a4a78cb61418438f35471308640c373074f4abbf
iNDECxdd <142155520+iNDECxdd@users.noreply.github.com>
2026-05-12 21:47:31 +0200

    Refactor user creation script for clarity and structure

--- create_users.sh
+++ create_users.sh
@@ -1,37 +1,40 @@
-#!/bin/bash 
+#!/bin/bash
 
-# Skapar användare med hemkataloger och en välkomstfil
+#Kollar om användaren är root eller inte
+if [ "$EUID" -ne 0 ]; then
+    echo "Du är inte root"
+    exit 1
+fi
 
-# Kollar om användaren är root eller inte
-if [ "$EUID" -ne 0 ]; then 
-    echo "Du har ej behörighet att köra detta script"
-    exit 1 
-fi 
-
-# Skapar nya användare och mappar, for-loop används för att gå igenom alla parametrar i $@
+#Skapar nya användare och mappar, for-loop används för att gå igenom alla parametrar i $@
 for USERNAME in "$@"; do 
-    # Skapar användare med en hemkatalog
-    useradd -m "$USERNAME" 
-    HOMEDIR="/home/$USERNAME" 
-    WELCOMEFILE="$HOMEDIR/welcome.txt"
+    #Skapar användare med en hemkatalog
+    useradd -m "$USERNAME"
+    HOME_DIRECTORY="/home/$USERNAME"
+    WELCOME="$HOME_DIRECTORY/welcome.txt"
 
-    # Skapar mappar i hemkatalogen 
-    mkdir -p "$HOMEDIR/Documents" 
-    mkdir -p "$HOMEDIR/Downloads" 
-    mkdir -p "$HOMEDIR/Work"
+    #Skapar mappar i hemkatalogen
+    mkdir -p "$HOME_DIRECTORY/Downloads"
+    mkdir -p "$HOME_DIRECTORY/Documents"
+    mkdir -p "$HOME_DIRECTORY/Work"
 
-    # Ger ägande över hemkatalogen till användaren
-    chown -R "$USERNAME:$USERNAME" "$HOMEDIR"
+    #Ger ägande över hemkatalogen till användaren
+    chown -R "$USERNAME:$USERNAME" "$HOME_DIRECTORY"
 
-    # Bara användaren kommer åt hemkatalogen 
-    chmod -R 700 "$HOMEDIR" 
+    #Bara användaren kommer åt hemkatalogen
+    chmod -R 700 "$HOME_DIRECTORY"
 
-    # Skapar välkomstfilen, for-loopen lägger till en lista över övriga användare från inparametrarna "$@", exklusive den aktuella användaren
-    echo "Välkommen $USERNAME" > "$WELCOMEFILE" 
-    echo "" >> "$WELCOMEFILE" 
-    echo "Andra användare:" >> "$WELCOMEFILE" 
-    awk -F: '$3 >= 1000 {print $1}' /etc/passwd | grep -v "$USERNAME" >> "$WELCOMEFILE"
+    #Skapar välkomstfilen, for-loopen lägger till en lista över övriga användare från inparametrarna "$@", exklusive den aktuella användaren
+    echo "Välkommen $USERNAME" > "$WELCOME"
+    echo "" >> "$WELCOME"
+    echo "Övriga användare:" >> "$WELCOME"
+    for user in "$@"; do
+        if [ "$user" != "$USERNAME" ]; then
+        echo "$user" >> "$WELCOME"
+        fi
+    done
 
-    # Ändrar rättigheter så alla kan läsa filen men bara ägaren kan ändra
-    chmod 644 "$WELCOMEFILE" 
-done 
+    #Ändrar rättigheter så alla kan läsa filen men bara ägaren kan ändra
+    chmod 644 "$WELCOME"
+
+done
