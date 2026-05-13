d402a57173f08cd6e68d1ef78e9aaf1c6a4c0c5c
iNDECxdd <142155520+iNDECxdd@users.noreply.github.com>
2026-05-13 10:50:09 +0200

    Provar att ta bort kommentarer och loop för att se hur rättningen ser ut
    Removed user creation loop and related functionality.

--- create_users.sh
+++ create_users.sh
@@ -1,26 +1,8 @@
 #!/bin/bash
 
-# Skriver ut användarens ID
 echo $EUID
 
-# Kollar om användarens ID är 0 (root) och stoppar scriptet om det inte är root
 if [ $EUID -ne 0 ]; then
     echo "Måste köras som root"
     exit 1
 fi
-
-# Loopar genom alla parametrar och skapar användare samt hemkatalog med tillhörande mappar och välkomstfil
-for USERNAME in $@; do
-    echo "Skapar användare $USERNAME"
-    # Skapar användare med hemkatalog (-m)
-    useradd -m $USERNAME
-
-    HOMEDIRECTORY="/home/$USERNAME"
-    WELCOMETEXT="$HOMEDIRECTORY/welcome.txt"
-
-    # Skapar mappar i hemkatalogen
-    mkdir -p "$HOMEDIRECTORY/Downloads" "$HOMEDIRECTORY/Documents" "$HOMEDIRECTORY/Work"
-
-    echo "Välkommen $USERNAME" >> "$WELCOMETEXT"
-
-done
