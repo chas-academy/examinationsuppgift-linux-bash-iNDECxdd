bd438706a9121ba794070336675f26633b04e484
iNDECxdd <142155520+iNDECxdd@users.noreply.github.com>
2026-05-13 13:53:34 +0200

    Update create_users.sh
    Added user creation with directories and permissions handling. Improved logging and added welcome message functionality.

--- create_users.sh
+++ create_users.sh
@@ -1,23 +1,43 @@
 #!/bin/bash
 
+# get user id of curent user
 user_id=$(id -u) 
 
 # echo $user_id
 
+# checks if user id is not 0
 if [[ $user_id -ne 0 ]]; then
     echo "Need to run as root"
     exit 1
 fi
 
-mkdir /log/
+echo "is root"
+mkdir /log/ # skapar ifall finns
 
-for user in $@; do
-    adduser $user 
-    mkdir -p "/home/$user/Documents" "/home/$user/Downloads" "/home/$user/Work"
-    echo "$user" >> /log/users
-    echo "Välkommen $user" > /home/$user/welcome.txt
-    # echo " " >> /home/$user/welcome.txt
-    # cat /log/users >> /home/$user/welcome.txt
-    
-    chown $user /home/$user/* && chgrp $user /home/$user/* && chmod 700 /home/$user/*
+echo ""
+
+# loop vi igen arguemt som gavs
+for usr in $@; do
+    echo "creating user $usr"
+    adduser --disabled-password --comment "" $usr 
+    echo creating Documents, Download and Work dirs
+    mkdir -p "/home/$usr/Documents" "/home/$usr/Downloads" "/home/$usr/Work"
+    echo "adding user to user log file" 
+    echo "$usr" >> /log/users
+    echo "Välkommen $usr" > /home/$usr/welcome.txt
+    # echo " " >> /home/$usr/welcome.txt
+    # cat /log/users >> /home/$usr/welcome.txt
+    echo "handeling user perms"
+    chown $usr /home/$usr/* && chgrp $usr /home/$usr/* && chmod 700 /home/$usr/* 
+    echo $(ls -lR /home/$usr/)
+    echo ""
+    echo ""
+done
+
+
+for usr in $@; do
+cat /log/users >> /home/$usr/welcome.txt
+cat /home/$usr/welcome.txt
+echo ""
+echo ""
 done
