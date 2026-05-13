deb9ac0fc5569fd57ed66d0eb59c12f8a8c4f510
iNDECxdd <142155520+iNDECxdd@users.noreply.github.com>
2026-05-13 10:31:45 +0200

    Tog bort "" vid for-loop för att se om det blir någon skillnad

--- create_users.sh
+++ create_users.sh
@@ -10,7 +10,7 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 # Loopar genom alla parametrar och skapar användare samt hemkatalog med tillhörande mappar och välkomstfil
-for USERNAME in "$@"; do
+for USERNAME in $@; do
     echo "Skapar användare $USERNAME"
     #Skapar användare med hemkatalog (-m)
     useradd -m "$USERNAME"
