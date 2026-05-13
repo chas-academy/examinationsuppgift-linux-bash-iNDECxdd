407bb2e88628adaad5c7e3a1011794550a601f96
iNDECxdd <142155520+iNDECxdd@users.noreply.github.com>
2026-05-13 12:03:09 +0200

    Tagit bort Shebang för att se om den fortfarande bockar av det iaf

--- create_users.sh
+++ create_users.sh
@@ -1,6 +1,3 @@
-#!/bin/bash
-
-# Loopar genom alla parametrar och skapar användare samt hemkatalog med tillhörande mappar och välkomstfil
 for USERNAME in $@; do
     echo "Skapar användare $USERNAME"
     #Skapar användare med hemkatalog (-m)
