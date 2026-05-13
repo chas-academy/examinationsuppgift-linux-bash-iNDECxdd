9738f11ff20b7c1f132d3b5f54c7ca3a330cb74b
iNDECxdd <142155520+iNDECxdd@users.noreply.github.com>
2026-05-13 13:18:07 +0200

    Enhance create_users.sh for root check and directories
    Updated user creation script to check for root privileges and create additional directories for new users.

--- create_users.sh
+++ create_users.sh
@@ -1,5 +1,21 @@
-for USERNAME in $@; do
-    echo "Skapar användare $USERNAME"
-    #Skapar användare med hemkatalog (-m)
-    useradd -m $USERNAME
+#!/bin/bash
+
+# Hämtar användarID
+USER_ID=$(id -u) 
+
+# echo $USER_ID
+
+# Kollar om användarens ID är 0
+if [[ $USER_ID -ne 0 ]]; then
+    echo "Måste köras som root"
+    exit 1
+fi
+
+# Loopar genom alla parametrar och skapar användare samt hemkatalog med tillhörande mappar och välkomstfil
+for user in $@; do
+    echo "Skapar användare $user"
+    adduser $user 
+    
+    mkdir -p "/home/$user/Documents" "/home/$user/Downloads" "/home/$user/Work"
+    
 done
